0d99bdf9c29b05dd05c8e3d19111d935f8c0bd71
Tien Nguyen Truong <tien.ngtruong@gmail.com>
2020-09-23 10:13:04 +0700

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,43 @@
-# vuetify-pagination-example
+# Vuetify Pagination (Server Side) example | v-pagination
+
+For more detail, please visit:
+> [Vuetify Pagination (Server Side) example](https://bezkoder.com/vuetify-pagination-server-side/)
+
+Server side Pagination for this app:
+> [Node.js Express Pagination with MySQL](https://bezkoder.com/node-js-sequelize-pagination-mysql/)
+
+> [Node.js Express Pagination with PostgreSQL](https://bezkoder.com/node-js-pagination-postgresql/)
+
+> [Node.js Express Pagination with MongoDB](https://bezkoder.com/node-js-mongodb-pagination/)
+
+> [Spring Boot Pagination and Filter example](https://bezkoder.com/spring-boot-pagination-filter-jpa-pageable/)
+
+> [Spring Boot MongoDB Pagination example with Spring Data](https://bezkoder.com/spring-boot-mongodb-pagination/)
+
+More Practice:
+> [Vuetify CRUD App with v-data-table](https://bezkoder.com/vuetify-data-table-example/)
+
+> [Vuetify File Upload example with Progress Bar](https://bezkoder.com/vuetify-file-upload/)
+
+Fullstack with Node.js Express:
+> [Vue.js + Node.js Express + MySQL](https://bezkoder.com/vue-js-node-js-express-mysql-crud-example/)
+
+> [Vue.js + Node.js Express + PostgreSQL](https://bezkoder.com/vue-node-express-postgresql/)
+
+> [Vue.js + Node.js Express + MongoDB](https://bezkoder.com/vue-node-express-mongodb-mevn-crud/)
+
+Fullstack with Spring Boot:
+> [Vue.js + Spring Boot](https://bezkoder.com/spring-boot-vue-js-crud-example/)
+
+> [Vue.js + Spring Boot + MongoDB](https://bezkoder.com/spring-boot-vue-mongodb/)
+
+Fullstack with Django:
+> [Vue.js + Django](https://bezkoder.com/django-vue-js-rest-framework/)
+
+Integration (run back-end & front-end on same server/port)
+> [Integrate Vue.js with Spring Boot](https://bezkoder.com/integrate-vue-spring-boot/)
+
+> [Integrate Vue App with Node.js Express](https://bezkoder.com/serve-vue-app-express/)
 
 ## Project setup
 ```
---
+++ src/components/TutorialsList.vue
@@ -0,0 +1,187 @@
+<template>
+  <v-row align="center" class="list px-3 mx-auto">
+    <v-col cols="12" sm="8">
+      <v-text-field v-model="searchTitle" label="Search by Title"></v-text-field>
+    </v-col>
+
+    <v-col cols="12" sm="4">
+      <v-btn small @click="page = 1; retrieveTutorials();">
+        Search
+      </v-btn>
+    </v-col>
+
+    <v-col cols="12" sm="12">
+      <v-row>
+        <v-col cols="4" sm="3">
+          <v-select
+            v-model="pageSize"
+            :items="pageSizes"
+            label="Items per Page"
+            @change="handlePageSizeChange"
+          ></v-select>
+        </v-col>
+
+        <v-col cols="12" sm="9">
+          <v-pagination
+            v-model="page"
+            :length="totalPages"
+            total-visible="7"
+            next-icon="mdi-menu-right"
+            prev-icon="mdi-menu-left"
+            @input="handlePageChange"
+          ></v-pagination>
+        </v-col>
+      </v-row>
+    </v-col>
+
+    <v-col cols="12" sm="12">
+      <v-card class="mx-auto" tile>
+        <v-card-title>Tutorials</v-card-title>
+
+        <v-data-table
+          :headers="headers"
+          :items="tutorials"
+          disable-pagination
+          :hide-default-footer="true"
+        >
+          <template v-slot:[`item.actions`]="{ item }">
+            <v-icon small class="mr-2" @click="editTutorial(item.id)">
+              mdi-pencil
+            </v-icon>
+            <v-icon small @click="deleteTutorial(item.id)">
+              mdi-delete
+            </v-icon>
+          </template>
+        </v-data-table>
+
+        <v-card-actions v-if="tutorials.length > 0">
+          <v-btn small color="error" @click="removeAllTutorials">
+            Remove All
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import TutorialDataService from "../services/TutorialDataService";
+export default {
+  name: "tutorials-list",
+  data() {
+    return {
+      tutorials: [],
+      searchTitle: "",
+      headers: [
+        { text: "Title", align: "start", sortable: false, value: "title" },
+        { text: "Description", value: "description", sortable: false },
+        { text: "Status", value: "status", sortable: false },
+        { text: "Actions", value: "actions", sortable: false },
+      ],
+
+      page: 1,
+      totalPages: 0,
+      pageSize: 3,
+
+      pageSizes: [3, 6, 9],
+    };
+  },
+  methods: {
+    getRequestParams(searchTitle, page, pageSize) {
+      let params = {};
+
+      if (searchTitle) {
+        params["title"] = searchTitle;
+      }
+
+      if (page) {
+        params["page"] = page - 1;
+      }
+
+      if (pageSize) {
+        params["size"] = pageSize;
+      }
+
+      return params;
+    },
+
+    retrieveTutorials() {
+      const params = this.getRequestParams(
+        this.searchTitle,
+        this.page,
+        this.pageSize
+      );
+
+      TutorialDataService.getAll(params)
+        .then((response) => {
+          const { tutorials, totalPages } = response.data;
+          this.tutorials = tutorials.map(this.getDisplayTutorial);
+          this.totalPages = totalPages;
+
+          console.log(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+
+    handlePageChange(value) {
+      this.page = value;
+      this.retrieveTutorials();
+    },
+
+    handlePageSizeChange(size) {
+      this.pageSize = size;
+      this.page = 1;
+      this.retrieveTutorials();
+    },
+
+    refreshList() {
+      this.retrieveTutorials();
+    },
+
+    removeAllTutorials() {
+      TutorialDataService.deleteAll()
+        .then((response) => {
+          console.log(response.data);
+          this.refreshList();
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+
+    editTutorial(id) {
+      this.$router.push({ name: "tutorial-details", params: { id: id } });
+    },
+
+    deleteTutorial(id) {
+      TutorialDataService.delete(id)
+        .then(() => {
+          this.refreshList();
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+
+    getDisplayTutorial(tutorial) {
+      return {
+        id: tutorial.id,
+        title: tutorial.title.substr(0, 20) + "...",
+        description: tutorial.description.substr(0, 20) + "...",
+        status: tutorial.published ? "Published" : "Pending",
+      };
+    },
+  },
+  mounted() {
+    this.retrieveTutorials();
+  },
+};
+</script>
+
+<style>
+.list {
+  max-width: 750px;
+}
+</style>
